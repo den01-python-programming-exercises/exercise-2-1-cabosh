3213af398df5a851a42aa66f8e2779e31b7f1de3
cabosh <callum.bosher@gmail.com>
2020-06-04 10:01:23 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
 
+    number = int(input("Enter a number: "))
+    print("Your number squared is " + str(number ** 2))
+
 if __name__ == '__main__':
     main()
